1a5b200cbc7d1b90b7ebc9b789a49589eb1cbfd2
KaioPinto <kaioricardo76@gmail.com>
2024-06-21 16:43:39 -0300

    alterações de volta

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-// Configuração do CORS mais granular
-const corsOptions = {
-  origin: "http://3.144.91.203", // Substitua pelo domínio do seu front-end em produção
-  methods: ["GET", "POST"], // Métodos HTTP permitidos
-  allowedHeaders: ["Content-Type"], // Cabeçalhos permitidos
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
 // DB Connection
 const conn = require("./db/conn");
+
 conn();
 
 // Routes
 const routes = require("./routes/router");
+
 app.use("/api", routes);
 
 app.listen(3000, function () {
